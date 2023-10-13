91e2e7d6ad91df7f83918cd4dd5edfe49e85f243
Evan <1585143453@qq.com>
2023-10-13 19:08:13 +0800

    radar_data

--- include/raw_data/radar_data.h
+++ include/raw_data/radar_data.h
@@ -6,18 +6,26 @@
 #include <fstream>
 
 #include "opencv2/opencv.hpp"
+#include "pcl/point_cloud.h"
+#include "pcl/point_types.h"
 
 namespace rawdata{
 
 class RadarData{
+private:
+    using POINT = pcl::PointXYZI;
+    using CLOUD = pcl::PointCloud<POINT>;
+    using CLOUDPTR = CLOUD::Ptr;
 public:
     RadarData(std::string &radar_data_path, std::string &radar_data_name);
+    CLOUDPTR trans_to_point_cloud();
 
 private:
-    const std::string cur_radar_data_timestamp;             // 文件时间戳
-    std::vector<float> cur_radar_data_row_timestamp;        // 每行数据时间戳
+    int64 cur_radar_data_timestamp;                         // 文件时间戳
+    std::vector<int64> cur_radar_data_row_timestamp;        // 每行数据时间戳
     std::vector<float> cur_radar_data_row_theta;            // 每行角度
     std::vector<std::vector<float>> cur_radar_data_raw;     // 原始幅度值
+    std::vector<std::vector<bool>> cur_radar_data_flag;     // 标志对应的幅度值是否有效
 
 }; // class RadarData
 
--- src/raw_data/radar_data.cpp
+++ src/raw_data/radar_data.cpp
@@ -3,14 +3,45 @@
 namespace rawdata{
 
 RadarData::RadarData(std::string &radar_data_path, std::string &radar_data_name)
-    : cur_radar_data_timestamp(radar_data_name)
 {
+    cur_radar_data_timestamp = std::stoll(radar_data_name);
     std::string radar_data_file_path = radar_data_path + "/" + radar_data_name + "png";
     cv::Mat fft_data = cv::imread(radar_data_file_path, 0);
-    cur_radar_data_row_theta.reserve(400);
-    cur_radar_data_row_timestamp.reserve(400);
-    cur_radar_data_raw.resize(400, std::vector<float>(4000, -1));
-    
+    cur_radar_data_row_theta.resize(fft_data.rows, 0);
+    cur_radar_data_row_timestamp.resize(fft_data.rows, 0);
+    cur_radar_data_raw.resize(fft_data.rows, std::vector<float>(fft_data.cols - 11, 0));
+    cur_radar_data_flag.resize(fft_data.rows, std::vector<bool>(fft_data.cols - 11, true));
+    static const float encode = M_PI / 2800.0;
+    for(int i = 0; i < fft_data.rows; ++i){
+        for(int j = 0; j < 8; ++j){
+            int64 tmp = (int64) fft_data.at<uchar>(i, j);
+            cur_radar_data_row_timestamp[i] = (cur_radar_data_row_timestamp[i] << 8) + tmp;
+        }
+        uint16_t high_byte = (uint16_t)fft_data.at<uchar>(i, 8);
+        uint16_t low_byte = (uint16_t)fft_data.at<uchar>(i, 9);
+        cur_radar_data_row_theta[i] = (float)((high_byte << 8) + low_byte);
+        cur_radar_data_row_theta[i] *= encode;
+        for(int j = 11; j < fft_data.cols; ++j){
+            cur_radar_data_raw[i][j - 11] = (float)fft_data.at<uchar>(i, j) / 255.0;
+        }
+    }
+}
+
+RadarData::CLOUDPTR RadarData::trans_to_point_cloud(){
+    CLOUDPTR point_cloud(new CLOUD());
+    point_cloud -> points.reserve(cur_radar_data_raw.size() * cur_radar_data_raw.back().size());
+    for(size_t i = 0; i < cur_radar_data_row_theta.size(); ++i){
+        float sin_theta = sin(cur_radar_data_row_theta[i]);
+        float cos_theta = cos(cur_radar_data_row_theta[i]);
+        for(size_t j = 0; j < cur_radar_data_raw.back().size(); ++j){
+            if(cur_radar_data_flag[i][j]){
+                float dis = (j + 1) * 0.0438;
+                POINT point(dis * cos_theta, dis * sin_theta, 0.0, cur_radar_data_raw[i][j]);
+                point_cloud -> push_back(point);
+            }
+        }
+    }
+    return point_cloud;
 }
 
 } // namespace rawdata
