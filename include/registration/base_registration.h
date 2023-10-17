63499243af2b9b7708e4fadbafc834622717cae1
Evan <1585143453@qq.com>
2023-10-17 21:34:13 +0800

    update

--- include/registration/base_registration.h
+++ include/registration/base_registration.h
@@ -1,6 +1,8 @@
 #ifndef _RADAR_LOCALIZATION_BASE_REGISTRATION
 #define _RADAR_LOCALIZATION_BASE_REGISTRATION
 
+#include <Eigen/Dense>
+
 namespace registration{
 
 class BaseRegistration
@@ -8,11 +10,6 @@ class BaseRegistration
 public:
     virtual void registration() {};
 }; // class BaseRegistration
-
-class NDT2d{
-
-
-}; // class NDT2d
 
 } // namespace registration
 
